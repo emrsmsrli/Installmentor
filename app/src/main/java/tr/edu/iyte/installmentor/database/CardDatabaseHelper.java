d5af4f034b8a7099b00b04d5881a2b35a0b94ba9
Emre <emrsmsrli1@gmail.com>
2017-03-17 23:23:43 +0300

    change constants with enum and include remaining amount to products

--- app/src/main/java/tr/edu/iyte/installmentor/database/CardDatabaseHelper.java
+++ app/src/main/java/tr/edu/iyte/installmentor/database/CardDatabaseHelper.java
@@ -24,10 +24,12 @@ public class CardDatabaseHelper extends SQLiteOpenHelper {
 
     private static final String TAG = CardDatabaseHelper.class.getSimpleName();
 
-    private static final int MODE_CREATE = 0;
-    private static final int MODE_READ = 1;
-    private static final int MODE_UPDATE = 2;
-    private static final int MODE_DELETE = 3;
+    private enum Mode {
+        CREATE,
+        READ,
+        UPDATE,
+        DELETE
+    }
 
     private static final String DB_NAME = "carddb";
     private static final String ID = "_id";
@@ -41,7 +43,7 @@ public class CardDatabaseHelper extends SQLiteOpenHelper {
     private static final String PRODUCT_CARD_ID = "pro_card";
     private static final String PRODUCT_DESCRIPTION = "pro_des";
     private static final String PRODUCT_BUY_DATE = "pro_buydate";
-    private static final String PRODUCT_TOTAL_AMOUNT = "pro_buydate";
+    private static final String PRODUCT_TOTAL_AMOUNT = "pro_totalamount";
 
     private static final String INSTALLMENT_TABLE_NAME = "installments";
     private static final String INSTALLMENT_PRODUCT_ID = "ins_pro";
@@ -118,61 +120,61 @@ public class CardDatabaseHelper extends SQLiteOpenHelper {
 
     public boolean addCard(Card card) {
         long id = db.insert(CARD_TABLE_NAME, null, initCard(card));
-        log(id, Card.class, MODE_CREATE);
+        log(id, Card.class, Mode.CREATE);
         return id >= 0;
     }
 
     public boolean addProduct(Product product) {
         long id = db.insert(PRODUCT_TABLE_NAME, null, initProduct(product));
-        log(id, Product.class, MODE_CREATE);
+        log(id, Product.class, Mode.CREATE);
         return id >= 0;
     }
 
     public boolean addInstallment(Installment installment) {
         long id = db.insert(INSTALLMENT_TABLE_NAME, null, initInstallment(installment));
-        log(id, Installment.class, MODE_CREATE);
+        log(id, Installment.class, Mode.CREATE);
         return id >= 0;
     }
 
     public boolean deleteCard(Card card) {
         String cardId = Long.toString(card.getId());
         int d = db.delete(CARD_TABLE_NAME, "? = ?", new String[]{ID, cardId});
-        log(d, Card.class, MODE_DELETE);
+        log(d, Card.class, Mode.DELETE);
         return d > 0;
     }
 
     public boolean deleteProduct(Product product) {
         String productId = Long.toString(product.getId());
         int d = db.delete(PRODUCT_TABLE_NAME, "? = ?", new String[]{ID, productId});
-        log(d, Product.class, MODE_DELETE);
+        log(d, Product.class, Mode.DELETE);
         return d > 0;
     }
 
     public boolean deleteInstallment(Installment installment) {
         String installmentId = Long.toString(installment.getId());
         int d = db.delete(INSTALLMENT_TABLE_NAME, "? = ?", new String[]{ID, installmentId});
-        log(d, Installment.class, MODE_DELETE);
+        log(d, Installment.class, Mode.DELETE);
         return d > 0;
     }
 
     public boolean editCard(Card card) {
         String cardId = Long.toString(card.getId());
         int u = db.update(CARD_TABLE_NAME, initCard(card), "? = ?", new String[]{ID, cardId});
-        log(u, Card.class, MODE_UPDATE);
+        log(u, Card.class, Mode.UPDATE);
         return u > 0;
     }
 
     public boolean editProduct(Product product) {
         String productId = Long.toString(product.getId());
         int u = db.update(PRODUCT_TABLE_NAME, initProduct(product), "? = ?", new String[]{ID, productId});
-        log(u, Product.class, MODE_UPDATE);
+        log(u, Product.class, Mode.UPDATE);
         return u > 0;
     }
 
     public boolean editInstallment(Installment installment) {
         String installmentId = Long.toString(installment.getId());
         int u = db.update(INSTALLMENT_TABLE_NAME, initInstallment(installment), "? = ?", new String[]{ID, installmentId});
-        log(u, Installment.class, MODE_UPDATE);
+        log(u, Installment.class, Mode.UPDATE);
         return u > 0;
     }
 
@@ -184,33 +186,53 @@ public class CardDatabaseHelper extends SQLiteOpenHelper {
                 cards.add(new Card(c.getLong(0), c.getString(1), c.getString(2), c.getString(3)));
             }
         }
-        log(cards.size(), Card.class, MODE_READ);
+        log(cards.size(), Card.class, Mode.READ);
         return cards;
     }
 
     public List<Product> getAllProducts() {
         ArrayList<Product> products;
-        try(Cursor c = db.query(PRODUCT_TABLE_NAME, null, null, null, null, null, null)) {
+        try(Cursor productC = db.query(PRODUCT_TABLE_NAME, null, null, null, null, null, null)) {
             products = new ArrayList<>();
-            while(c.moveToNext()) {
-                Date d = DateFormatBuilder.parse(c.getString(4));
-                products.add(new Product(c.getLong(0), c.getLong(1), c.getLong(2), c.getString(3), d));
+            while(productC.moveToNext()) {
+                Date d = DateFormatBuilder.parse(productC.getString(4));
+                Product p = new Product(productC.getLong(0), productC.getLong(1), productC.getLong(2), productC.getString(3), d);
+
+                try(Cursor installmentC = db.rawQuery(
+                        "SELECT SUM(" + INSTALLMENT_AMOUNT + ") " +
+                                "FROM " + INSTALLMENT_TABLE_NAME +
+                                " WHERE " + INSTALLMENT_PRODUCT_ID +" = " + p.getId() + ";", null)) {
+                    if(installmentC.moveToNext())
+                        p.setRemainingAmount(installmentC.getFloat(0));
+                }
+
+                products.add(p);
             }
         }
-        log(products.size(), Product.class, MODE_READ);
+        log(products.size(), Product.class, Mode.READ);
         return products;
     }
 
     public List<Product> getProductsOf(Card card) {
         ArrayList<Product> products;
-        try(Cursor c = db.query(PRODUCT_TABLE_NAME, null, "? = ?", new String[]{PRODUCT_CARD_ID, String.valueOf(card.getId())}, null, null, null)) {
+        try(Cursor productC = db.query(PRODUCT_TABLE_NAME, null, "? = ?", new String[]{PRODUCT_CARD_ID, String.valueOf(card.getId())}, null, null, null)) {
             products = new ArrayList<>();
-            while(c.moveToNext()) {
-                Date d = DateFormatBuilder.parse(c.getString(4));
-                products.add(new Product(c.getLong(0), c.getLong(1), c.getLong(2), c.getString(3), d));
+            while(productC.moveToNext()) {
+                Date d = DateFormatBuilder.parse(productC.getString(4));
+                Product p = new Product(productC.getLong(0), productC.getLong(1), productC.getLong(2), productC.getString(3), d);
+
+                try(Cursor installmentC = db.rawQuery(
+                        "SELECT SUM(" + INSTALLMENT_AMOUNT + ") " +
+                                "FROM " + INSTALLMENT_TABLE_NAME +
+                                " WHERE " + INSTALLMENT_PRODUCT_ID +" = " + p.getId() + ";", null)) {
+                    if(installmentC.moveToNext())
+                        p.setRemainingAmount(installmentC.getFloat(0));
+                }
+
+                products.add(p);
             }
         }
-        log(products.size(), Product.class, MODE_READ);
+        log(products.size(), Product.class, Mode.READ);
         return products;
     }
 
@@ -223,7 +245,7 @@ public class CardDatabaseHelper extends SQLiteOpenHelper {
                 installments.add(new Installment(c.getLong(0), c.getLong(1), c.getLong(2), c.getFloat(3), d));
             }
         }
-        log(installments.size(), Installment.class, MODE_READ);
+        log(installments.size(), Installment.class, Mode.READ);
         return installments;
     }
 
@@ -236,7 +258,7 @@ public class CardDatabaseHelper extends SQLiteOpenHelper {
                 installments.add(new Installment(c.getLong(0), c.getLong(1), c.getLong(2), c.getFloat(3), d));
             }
         }
-        log(installments.size(), Installment.class, MODE_READ);
+        log(installments.size(), Installment.class, Mode.READ);
         return installments;
     }
 
@@ -249,7 +271,7 @@ public class CardDatabaseHelper extends SQLiteOpenHelper {
                 installments.add(new Installment(c.getLong(0), c.getLong(1), c.getLong(2), c.getFloat(3), d));
             }
         }
-        log(installments.size(), Installment.class, MODE_READ);
+        log(installments.size(), Installment.class, Mode.READ);
         return installments;
     }
 
@@ -307,25 +329,25 @@ public class CardDatabaseHelper extends SQLiteOpenHelper {
         }.execute();
     }
 
-    private void log(long i, Class<?> c, int mode) {
+    private void log(long i, Class<?> c, Mode mode) {
         String cn = c.getSimpleName();
         switch(mode) {
-            case MODE_CREATE:
+            case CREATE:
                 if(i < 0)
                     Log.e(TAG, "add" + cn + ": Error creating " + cn);
                 else
                     Log.i(TAG, "add" + cn + ": new " + cn + " created, id: " + i);
                 break;
-            case MODE_READ:
+            case READ:
                 Log.i(TAG, "get" + cn + ": Read count: " + i);
                 break;
-            case MODE_UPDATE:
+            case UPDATE:
                 if(i <= 0)
                     Log.e(TAG, "edit" + cn + ": Couldn't edit " + cn);
                 else
                     Log.i(TAG, "edit" + cn + ": " + cn + " edited");
                 break;
-            case MODE_DELETE:
+            case DELETE:
                 if(i <= 0)
                     Log.e(TAG, "delete" + cn + ": Couldn't delete " + cn);
                 else
